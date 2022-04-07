ac8ffb8638503da28403a4eee0a602f8449a68a1
HaiTing-Zhu <236467557@qq.com>
2022-04-07 18:05:36 +0800

    create coderslang

---
+++ DONE/task85/solution.js
@@ -0,0 +1,7 @@
+function printUppercaseMessage (message) {
+  console.log(message.toUpperCase());
+}
+
+const myMessage = `I'm on a path to becoming a JS developer!`;
+
+printUppercaseMessage(myMessage);
---
+++ DONE/task87/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+const names = "hudeam";
+const isClick = true
+const hasObj = false
+const priceAmount = 12
+const age = 20
